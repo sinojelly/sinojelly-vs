081e836f6728accb44fd394d3a42b5abb6fc9fe2
Chen Guodong <sinojelly@163.com>
2010-11-14 02:03:16 +0800

    macro support test file like *Test.h

--- trunk/TestngppAddin/TestngppAddin/TestInfoParser.cs
+++ trunk/TestngppAddin/TestngppAddin/TestInfoParser.cs
@@ -14,11 +14,36 @@ namespace TestngppAddin
         string depends;
     }
 
+    class FixtureInfo
+    {
+        public string name;
+        public int index;
+    }
+
+    class Fixture
+    {
+        private List<FixtureInfo> fixtures;
+        
+        public string this[int index]
+        {
+            get
+            {
+                for (int i = 0; i < index; i++)
+                {
+                
+                }
+                return fixtures.
+            }
+        }
+    }
+
     /// <summary>
     /// 解析一个用例的信息
     /// </summary>
     public class TestInfoParser
     {
+        public Fixture Fixtures;
+
         public bool parse(Stream stream)
         {
             int count = 0;
